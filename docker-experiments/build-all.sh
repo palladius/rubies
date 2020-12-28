11b3fe86875035cdcc055402f492b4267caaa041
Riccardo Carlesso <ricc+derek.zrh@google.com>
2020-12-28 07:45:40 +0100

    my favorite

--- docker-experiments/build-all.sh
+++ docker-experiments/build-all.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+VER=1.2
+echo "$0 ver$VER. Starting.."
+
 for F in Dockerfile.* ; do 
     SUFFIX=$(echo $F | cut -d. -f 2 )
     if [ -f  ".$F.ok" ]; then
         verde "Image $F already exists. Skipping"
     else
-    white "== Prcoessing $F =="
-    docker build -t riccruby:$SUFFIX - < Dockerfile.$SUFFIX &&
+    white "== Processing: $F =="
+    #| tee ".$F.log"
+    docker build -t riccruby:$SUFFIX -f Dockerfile.$SUFFIX . 2>&1 | tee ".$F.log" &&
         touch ".$F.ok" ||
             touch ".$F.nope"
     fi
